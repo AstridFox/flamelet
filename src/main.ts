4dfddd5e105483d2986a9c364d37e27258365b6d
Erika Foxelot <erikafoxelot@gmail.com>
2025-05-29 10:38:28 -0400

    Add new test preset

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import './style.css';
 import { renderFlame } from './renderer';
 import type { FlamePreset } from './types';
-import basicPreset from '../presets/basic.json';
+import basicPreset from '../presets/orbit-distance.json';
 
 const app = document.querySelector<HTMLDivElement>('#app');
 if (!app) {
